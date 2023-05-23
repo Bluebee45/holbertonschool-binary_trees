e1915d69f4353c45bb751f8184df81ad0cbb4fb7
Bluebee45 <santi72143@gmail.com>
2023-05-23 16:25:18 -0500

    fix file

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -8,22 +8,22 @@
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	size_t l_ht, r_h;
+	size_t l_h, r_h;
 
 	if (tree == NULL)
 		return (0);
 
-	l_h = binary_tree_height(tree->left);
-	r_h = binary_tree_height(tree->right);
+	l_h = binary_tree_height(tree->l);
+	r_h = binary_tree_height(tree->r);
 
 	if (l_h != r_h)
 		return (0);
 
-	if (tree->left == NULL && tree->right == NULL)
+	if (tree->l == NULL && tree->r == NULL)
 		return (1);
 
-	return (binary_tree_is_perfect(tree->left) &&
-	binary_tree_is_perfect(tree->right));
+	return (binary_tree_is_perfect(tree->l) &&
+	binary_tree_is_perfect(tree->r));
 }
 
 /**
@@ -39,8 +39,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	l_h = binary_tree_height(tree->left);
-	r_h = binary_tree_height(tree->right);
+	l_h = binary_tree_height(tree->l);
+	r_h = binary_tree_height(tree->r);
 
 	return (1 + (l_h > r_h ? l_h : r_h));
 }
