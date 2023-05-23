dd167c53a378f75c52c6914afd0e60ef7ad3d698
Bluebee45 <santi72143@gmail.com>
2023-05-23 16:28:28 -0500

    fix file

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -8,22 +8,22 @@
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	size_t l_h, r_h;
+	size_t left_height, right_height;
 
 	if (tree == NULL)
 		return (0);
 
-	l_h = binary_tree_height(tree->l);
-	r_h = binary_tree_height(tree->r);
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
 
-	if (l_h != r_h)
+	if (left_height != right_height)
 		return (0);
 
-	if (tree->l == NULL && tree->r == NULL)
+	if (tree->left == NULL && tree->right == NULL)
 		return (1);
 
-	return (binary_tree_is_perfect(tree->l) &&
-	binary_tree_is_perfect(tree->r));
+	return (binary_tree_is_perfect(tree->left) &&
+	binary_tree_is_perfect(tree->right));
 }
 
 /**
@@ -34,13 +34,13 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t l_h, r_h;
+	size_t left_height, right_height;
 
 	if (tree == NULL)
 		return (0);
 
-	l_h = binary_tree_height(tree->l);
-	r_h = binary_tree_height(tree->r);
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
 
-	return (1 + (l_h > r_h ? l_h : r_h));
+	return (1 + (left_height > right_height ? left_height : right_height));
 }
